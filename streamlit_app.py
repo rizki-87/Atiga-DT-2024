8817f381d6ef81080ea38947cf58e5dddd961562
rizki-87 <140106584+rizki-87@users.noreply.github.com>
2024-03-20 08:59:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,18 @@
 import streamlit as st
-import gspread
 import pandas as pd
+import requests
 
 # Function to get data from Google Spreadsheet
 def get_data():
-    # Open the public spreadsheet
-    gc = gspread.Client()
-    sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1u8W2CTCgSSQAFuci59bMmFczQyH3_7ajgCRJJH9CNQE')
-    worksheet = sh.worksheet('Combined Sheet')
-    data = worksheet.get_all_records()
-    df = pd.DataFrame(data)
+    # Your spreadsheet's published CSV URL
+    url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTnflGSDkG_l9mSnawp-HEHX-R5jMfluS1rp0HlF_hMBpQvtG21d3-zPE4TxD80CvQVPjJszeOmNWJB/pub?gid=384797514&single=true&output=csv'
+    
+    # Send a HTTP GET request to the URL
+    response = requests.get(url)
+    assert response.status_code == 200, 'Wrong status code'  # Check to make sure the request went through
+
+    # Read the CSV content into a DataFrame
+    df = pd.read_csv(url)
     return df
 
 # Display data in Streamlit
