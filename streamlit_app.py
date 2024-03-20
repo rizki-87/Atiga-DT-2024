78e2a8d923c08eaea5e7a7141b3f5d7809160d83
rizki-87 <140106584+rizki-87@users.noreply.github.com>
2024-03-20 11:57:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,57 +2,29 @@ import streamlit as st
 import pandas as pd
 import requests
 
-# Membersihkan cache
-st.legacy_caching.clear_cache()
-
 # Function to get data from Google Spreadsheet
 def get_data():
-    # Your spreadsheet's published CSV URL
-    url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTnflGSDkG_l9mSnawp-HEHX-R5jMfluS1rp0HlF_hMBpQvtG21d3-zPE4TxD80CvQVPjJszeOmNWJB/pub?gid=384797514&single=true&output=csv'
-    
-    # Send a HTTP GET request to the URL
+    url = 'YOUR_SPREADSHEET_URL'
     response = requests.get(url)
     assert response.status_code == 200, 'Wrong status code'
-
-    # Read the CSV content into a DataFrame
     df = pd.read_csv(url)
-    
-    # Convert the 'TAHUN DT' column to numeric, coercing errors to NaN
-    df['TAHUN DT'] = pd.to_numeric(df['TAHUN DT'], errors='coerce')
-    
-    # Fill NaN values with a value of your choice or drop them
-    df['TAHUN DT'] = df['TAHUN DT'].fillna(0).astype(int)
 
-    # Convert the 'TANGGAL' column to date objects
-    df['TANGGAL'] = pd.to_datetime(df['TANGGAL'], dayfirst=True, errors='coerce').dt.date
+    # Clean and convert the 'TAHUN DT' column
+    df['TAHUN DT'] = pd.to_numeric(df['TAHUN DT'], errors='coerce').fillna(0).astype(int)
 
+    # Convert the 'TANGGAL' column to dates
+    df['TANGGAL'] = pd.to_datetime(df['TANGGAL'], errors='coerce').dt.date
     return df
 
 # Display data in Streamlit
 st.title('Monitoring Ketersediaan & Kondisi Dump Truck')
-
-# Get the data
 df = get_data()
 
-# Assume 'TANGGAL' is your date column name
+# Check if 'TANGGAL' column exists and has no nulls
 if 'TANGGAL' in df.columns and not df['TANGGAL'].isnull().all():
-    # Extract dates for the slider
-    min_date = df['TANGGAL'].min()
-    max_date = df['TANGGAL'].max()
-
-    # Create the date slider
-    selected_date_range = st.slider(
-        "Pilih Rentang Tanggal",
-        value=(min_date, max_date),
-        format='DD-MM-YYYY'
-    )
-
-    # Filter the dataframe based on the selected date range
-    filtered_df = df[(df['TANGGAL'] >= selected_date_range[0]) & (df['TANGGAL'] <= selected_date_range[1])]
-
-    # Display the filtered data
-    # Format dates in the dataframe to 'DD-MM-YYYY' before displaying
-    filtered_df['TANGGAL'] = filtered_df['TANGGAL'].dt.strftime('%d-%m-%Y')
+    min_date, max_date = df['TANGGAL'].min(), df['TANGGAL'].max()
+    selected_date_range = st.slider("Pilih Rentang Tanggal", value=(min_date, max_date))
+    filtered_df = df[df['TANGGAL'].between(*selected_date_range)]
     st.write(filtered_df)
 else:
     st.error("Kolom 'TANGGAL' tidak ditemukan atau mengandung nilai null dalam dataframe.")
