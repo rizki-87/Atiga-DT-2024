f96327e6283a038380963f9e4ad54d8fc86972f3
rizki-87 <140106584+rizki-87@users.noreply.github.com>
2024-03-20 13:23:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,18 +5,21 @@ import requests
 # Function to get data from Google Spreadsheet
 def get_data():
     # Your spreadsheet's published CSV URL
-    url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTnflGSDkG_l9mSnawp-HEHX-R5jMfluS1rp0HlF_hMBpQvtG21d3-zPE4TxD80CvQVPjJszeOmNWJB/pub?gid=384797514&single=true&output=csv'
+    url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTnflGSDkG_l9mSnawp-HEHX-R5jMfluS1rp0HlF_hMBpQvtG21d3-zPE4TxD80CvQVPjJszeOmNWJB/pub?output=csv'
     
-    # Send a HTTP GET request to the URL
-    response = requests.get(url)
-    assert response.status_code == 200, 'Wrong status code'  # Check to make sure the request went through
-
     # Read the CSV content into a DataFrame
     df = pd.read_csv(url)
-    
-    # Convert the TAHUN DT column to integers
-    df['TAHUN DT'] = df['TAHUN DT'].apply(lambda x: int(x) if not pd.isnull(x) else x)
-    
+
+    # Convert the 'TAHUN DT' column to numeric, coercing errors to NaN
+    df['TAHUN DT'] = pd.to_numeric(df['TAHUN DT'], errors='coerce')
+
+    # Fill NaN values with a value of your choice or drop them
+    df['TAHUN DT'] = df['TAHUN DT'].fillna(0).astype(int) # Option 1: Fill with 0
+    # df = df.dropna(subset=['TAHUN DT'])  # Option 2: Drop rows with NaN in 'TAHUN DT'
+
+    # Now safely convert to integers
+    df['TAHUN DT'] = df['TAHUN DT'].astype(int)
+
     return df
 
 # Display data in Streamlit
