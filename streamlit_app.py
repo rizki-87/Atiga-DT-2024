b76f540b79830aa622501cbd503e48c37c28ee03
rizki-87 <140106584+rizki-87@users.noreply.github.com>
2024-03-20 13:03:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,27 +4,25 @@ import requests
 
 # Function to get data from Google Spreadsheet
 def get_data():
+    # Your spreadsheet's published CSV URL
     url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTnflGSDkG_l9mSnawp-HEHX-R5jMfluS1rp0HlF_hMBpQvtG21d3-zPE4TxD80CvQVPjJszeOmNWJB/pub?gid=384797514&single=true&output=csv'
+    
+    # Send a HTTP GET request to the URL
     response = requests.get(url)
-    assert response.status_code == 200, 'Wrong status code'
+    assert response.status_code == 200, 'Wrong status code'  # Check to make sure the request went through
+
+    # Read the CSV content into a DataFrame
     df = pd.read_csv(url)
-
-    # Clean and convert the 'TAHUN DT' column
-    df['TAHUN DT'] = pd.to_numeric(df['TAHUN DT'], errors='coerce').fillna(0).astype(int)
-
-    # Convert the 'TANGGAL' column to dates
-    df['TANGGAL'] = pd.to_datetime(df['TANGGAL'], errors='coerce').dt.date
     return df
 
 # Display data in Streamlit
-st.title('Monitoring Ketersediaan & Kondisi Dump Truck')
-df = get_data()
+st.title('Dashboard Visualisasi Data')
 
-# Check if 'TANGGAL' column exists and has no nulls
-if 'TANGGAL' in df.columns and not df['TANGGAL'].isnull().all():
-    min_date, max_date = df['TANGGAL'].min(), df['TANGGAL'].max()
-    selected_date_range = st.slider("Pilih Rentang Tanggal", value=(min_date, max_date))
-    filtered_df = df[df['TANGGAL'].between(*selected_date_range)]
-    st.write(filtered_df)
+# Button for refreshing data
+if st.button('Refresh Data'):
+    df = get_data()
+    st.write(df)
 else:
-    st.error("Kolom 'TANGGAL' tidak ditemukan atau mengandung nilai null dalam dataframe.")
+    # Display the initial data when the app is first run
+    df = get_data()
+    st.write(df)
